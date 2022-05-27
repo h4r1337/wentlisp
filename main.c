39e534502e57dfd6eca50792f7d3c71c918e1e1f
h4r1337 <ghostsquad139@gmail.com>
2022-05-27 21:24:17 +0530

    added editline for line editing

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Declare a buffer for user input of size 2048
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
+
 
 int main(int argc, char** argv) {
 
@@ -10,14 +12,17 @@ int main(int argc, char** argv) {
   puts("Press Ctrl+c to Exit\n");
 
   while(1){
-    // Prompt
-    fputs("went> ", stdout);
+    // R.I.P golang
+    char* input = readline("went> ");
 
-    // Read a line of user input of maximum size 2048
-    fgets(input, 2048, stdin);
+    // Add input to history
+    add_history(input);
 
     // Echo input back to user
-    printf("%s", input);
+    printf("%s\n", input);
+
+    // Free retrieved input
+    free(input);
   }
 
   return 0;
