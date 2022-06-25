ac79486bd362405dd18c252141de4412739f19fa
h4r1337 <ghostsquad139@gmail.com>
2022-06-25 17:15:38 +0530

    Evaluating the given expression (+, -, *, and / operators)

--- parsing.c
+++ parsing.c
@@ -24,6 +24,32 @@ void add_history(char unused) {}
 #include <editline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  char* op = t->children[1]->contents;
+
+  long x = eval(t->children[2]);
+
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv) {
 
   // Creating some parsers
@@ -43,7 +69,7 @@ int main(int argc, char** argv) {
     Number, Operator, Expr, Lispy);
 
   // print version and exit info
-  puts("Went Version 0.0.0.0.2");
+  puts("Went Version 0.0.0.0.3");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
@@ -54,9 +80,11 @@ int main(int argc, char** argv) {
     /* Attempt to parse the user input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      /* On success print and delete the AST */
-      mpc_ast_print(r.output);
+      
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
+
     } else {
       /* Otherwise print and delete the Error */
       mpc_err_print(r.error);
